4faa2ddf1be3ebff5cf5106f2fc17c68de422b2a
OliviaXu965 <67605616+OliviaXu965@users.noreply.github.com>
2023-03-07 15:43:05 -0500

    arduino

--- Experiment_code/arduinoTest.m
+++ Experiment_code/arduinoTest.m
@@ -12,11 +12,3 @@ fopen(arduino);
 fprintf(arduino,'%s',char(111));
 fclose(arduino);
 clear;
-%% Arduino test
-a= arduino();
-   for i = 1:10
-      writeDigitalPin(a, 'A11', 0);
-      pause(0.5);
-      writeDigitalPin(a, 'A11', 1);
-      pause(0.5);
-   end
