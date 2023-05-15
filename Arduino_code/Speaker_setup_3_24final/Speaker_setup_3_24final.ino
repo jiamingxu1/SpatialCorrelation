a00616527075c31dc4e25eff81f6e10b60d02372
OliviaXu965 <67605616+OliviaXu965@users.noreply.github.com>
2023-05-15 18:41:47 -0400

    made changes to speaker_pretest, in-between sound

--- Arduino_code/Speaker_setup_3_24final/Speaker_setup_3_24final.ino
+++ Arduino_code/Speaker_setup_3_24final/Speaker_setup_3_24final.ino
@@ -2,7 +2,7 @@ const int speakerPins[] = {10, 11, 8, 9, 6, 7, 4, 5, 13, 12, A4, A5, A1, A0, A2,
 const int numSpeakers = 16;
 
 void setup() {
-  Serial.begin(9600); // Start serial communication
+  Serial.begin(115200); // Start serial communication
   Serial.setTimeout(100);
   for (int i = 0; i < numSpeakers; i++) {
     pinMode(speakerPins[i], OUTPUT); // Set each speaker pin as output
@@ -14,34 +14,23 @@ void loop() {
 
   if (Serial.available()) {
     String input = Serial.readStringUntil('>');
-//    Serial.print(input);
     if (input.startsWith("<")) {
       input = input.substring(1, input.length()); // Remove start and end markers
-//      Serial.print(input);
         int colonIndex = input.indexOf(":");
-//        Serial.print(colonIndex);
-//        Serial.print(',');
         if (colonIndex > 0) {
           int state = input.substring(0, colonIndex).toInt(); //turn on or off
-//          Serial.print(state);
           input = input.substring(colonIndex+1); //first number after the colon
           
           while (input.length() > 0) {
             int commaIndex = input.indexOf(",");
             if (commaIndex < 0) {
               commaIndex = input.length();
-//              Serial.print(commaIndex);
-//              Serial.print(',');
             }
             int speakerIndex = input.substring(0, commaIndex).toInt() - 1; // Speaker indexes start from 1, so subtract 1
-//            Serial.print(speakerIndex);
-//            Serial.print(',');
             if (speakerIndex >= 0 && speakerIndex <= numSpeakers) { 
               digitalWrite(speakerPins[speakerIndex], state); 
             }
             input = input.substring(commaIndex+1);
-//            Serial.print(input);
-//            Serial.print(',');
         }
       }
     }
